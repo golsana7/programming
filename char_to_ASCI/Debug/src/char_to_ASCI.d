9ef27d19995004fc00b248fe99ea6f6c642a934f
golsana7 <golsanaaa@gmail.com>
2017-07-02 00:05:43 +0430

    commit operation

---
+++ char_to_ASCI/Debug/src/char_to_ASCI.d
@@ -0,0 +1,1 @@
+src/char_to_ASCI.d: ../src/char_to_ASCI.cpp
---
+++ char_to_ASCI/src/char_to_ASCI.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : char_to_ASCI.cpp
+// Author      : Golsana
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	char ascii;
+	cout << "!!!Char to ASCII converter!!!" << endl << endl;
+	cout << "please type a character:" << endl;
+	cin >> ascii;
+	cout << "Its ASCII value is: "<< int(ascii) << endl;
+	return 0;
+}
