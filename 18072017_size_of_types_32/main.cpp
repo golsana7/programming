9cefa1fe0227ae9159e1e86201eca51f1a028fac
golsana7 <golsanaaa@gmail.com>
2017-07-18 22:04:47 +0430

    commit 18072017_size_of_types_32

---
+++ 18072017_size_of_types_32/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jul 18, 2017
+ *      Author: golsana
+ */
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"size of int is: "<<sizeof(int)<<" bytes."<<endl;
+	cout<<"size of short int is: "<<sizeof(short int)<<" bytes."<<endl;
+	cout<<"size of long int is: "<<sizeof(long int)<<" bytes."<<endl;
+	cout<<"size of long long int is: "<<sizeof(long long int)<<" bytes."<<endl;
+	cout<<"size of unsigned int is: "<<sizeof(unsigned int)<<" bytes."<<endl;
+	cout<<"size of float is: "<<sizeof(float)<<" bytes."<<endl;
+	cout<<"size of double is: "<<sizeof(double)<<" bytes."<<endl;
+	cout<<"size of char is: "<<sizeof(char)<<" byte."<<endl;
+	return 0;
+}
